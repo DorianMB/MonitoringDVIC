8ea7f68d7d95cc2f133c802054c9eea5014ddfaf
Dorian <dorian.marques.2e7@gmail.com>
2020-04-03 10:53:28 +0200

    install dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import * as firebase from 'firebase';
 
 Vue.config.productionTip = false;
+
+const app = firebase.initializeApp({
+  apiKey: '<your-api-key>',
+  authDomain: '<your-auth-domain>',
+  databaseURL: '<your-database-url>',
+  projectId: '<your-cloud-firestore-project>',
+  storageBucket: '<your-storage-bucket>',
+  messagingSenderId: '<your-sender-id>'
+});
 
 new Vue({
   router,

2df28b0f9f5622125d356e7962513f27af05f5af
Dorian <dorian.marques.2e7@gmail.com>
2020-04-12 11:32:43 +0200

    clickable stepper

--- src/components/Stepper.vue
+++ src/components/Stepper.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="stepper-container">
     <template v-for="step in steps">
-      <div class="stepper-point" :key="step" :class="step === activeStep ? 'stepper-point-active' : ''"></div>
+      <div class="stepper-point" :key="step" :class="step === activeStep ? 'stepper-point-active' : ''" v-on:click="updateStep(step)"></div>
     </template>
   </div>
 </template>
@@ -17,6 +17,11 @@ export default {
     steps: {
       type: Number,
       default: 3
+    }
+  },
+  methods: {
+    updateStep(newStep) {
+      this.$emit("newStep", newStep);
     }
   }
 }
--- src/views/Inscription.vue
+++ src/views/Inscription.vue
@@ -1,30 +1,69 @@
 <template>
   <div>
-    <navbar :logo="false"></navbar>
-    <div class="inscription d-flex flex-column align-items-center">
-      <img class="mar-t-14 mar-b-8" src="../assets/images/logo-white.png" />
+    <navbar :logo="true"></navbar>
+    <div class="inscription d-flex flex-column justify-content-center align-items-center">
       <div class="card-layout d-flex flex-column align-items-center">
         <h1 class="mar-b-0">Inscription</h1>
-        <div class="group-input-label mar-t-2">
-          <label for="email">Email</label>
-          <input
-            type="text"
-            id="email"
-            placeholder="Email"
-            class="form-control"
-          />
+        <div v-if="currentStep === 1" id="step1" class="w-100 d-flex flex-column align-items-center">
+          <div class="group-input-label mar-t-2">
+            <label for="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              placeholder="Email"
+              class="form-control"
+            />
+          </div>
+          <div class="group-input-label mar-t-3">
+            <label for="password">Mot de passe</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Mot de passe"
+              class="form-control"
+            />
+          </div>
+          <div class="group-input-label mar-t-3">
+            <label for="password">Confirmer le mot de passe</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              placeholder="Confirmer le mot de passe"
+              class="form-control"
+            />
+          </div>
         </div>
-        <div class="group-input-label mar-t-3">
-          <label for="password">Mot de passe</label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Mot de passe"
-            class="form-control"
-          />
+        <div v-else-if="currentStep === 2" id="step2" class="w-100 d-flex flex-column align-items-center">
+          <div class="group-input-label mar-t-2">
+            <label for="firstname">Prénom du gérant</label>
+            <input
+              type="text"
+              id="firstname"
+              placeholder="Prénom"
+              class="form-control"
+            />
+          </div>
+          <div class="group-input-label mar-t-3">
+            <label for="lastname">Nom du gérant</label>
+            <input
+              type="text"
+              id="lastname"
+              placeholder="Nom"
+              class="form-control"
+            />
+          </div>
+          <div class="group-input-label mar-t-3">
+            <label for="phone">Numéro de téléphone</label>
+            <input
+              type="text"
+              id="phone"
+              placeholder="Téléphone"
+              class="form-control"
+            />
+          </div>
         </div>
-        <button class="btn btn-primary mar-t-4">Se connecter</button>
-        <stepper :steps="3" :active-step="2"></stepper>
+        <button class="btn btn-primary mar-t-4" v-on:click="nextStep()">Suivant</button>
+        <stepper :steps="4" :active-step="currentStep" @newStep="updateStep"></stepper>
       </div>
     </div>
   </div>
@@ -37,7 +76,20 @@ import Stepper from "@/components/Stepper";
 
 export default {
   name: "Inscription",
-  components: {Stepper, Navbar }
+  components: {Stepper, Navbar},
+  data: () => {
+    return {
+      currentStep: 1
+    };
+  },
+  methods: {
+    nextStep() {
+      this.currentStep ++;
+    },
+    updateStep(value) {
+      this.currentStep = value;
+    }
+  }
 };
 </script>
 
@@ -51,6 +103,7 @@ export default {
 
   .card-layout {
     width: 28%;
+
     .btn-primary {
       width: 60%;
     }
